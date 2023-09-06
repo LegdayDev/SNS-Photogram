5ca1ad2f4ef2b279249147b89e64111c94a3417d
Legday <chlwodud0327@naver.com>
2023-09-06 18:30:48 +0900

    UpdateForm 에 Session 데이터 삽입하기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -17,14 +18,19 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
-        // 1. 어노테이션을 이용한 쉬운 방법
-        System.out.println("세션 정보 : " + principalDetails.getUser());
+    public String update(@PathVariable int id,
+                         @AuthenticationPrincipal PrincipalDetails principalDetails,
+                         Model model){
+        /** Security Session 찾기
+         * 1. @AuthenticationPrincipal 어노테이션을 이용(권장)
+             System.out.println("세션 정보 : " + principalDetails.getUser());
+         * 2. 직접 역추적해서 찾는 방법(비추)
+             Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+             PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+             System.out.println("직접 찾은 세션 정보 : " + mPrincipalDetails.getUser());
+         */
 
-        // 2. 복잡하고 어려운 방법
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
-        System.out.println("직접 찾은 세션 정보 : " + mPrincipalDetails.getUser());
+        model.addAttribute("principal", principalDetails.getUser());
 
         return "user/update";
     }
