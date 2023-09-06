1e906bf525fc383f59993af1f349fa4ade67b2eb
Legday <chlwodud0327@naver.com>
2023-09-06 17:25:12 +0900

    Security Session 찾기

--- src/main/java/com/cos/photogramstart/web/UserController.java
+++ src/main/java/com/cos/photogramstart/web/UserController.java
@@ -1,5 +1,9 @@
 package com.cos.photogramstart.web;
 
+import com.cos.photogramstart.config.auth.PrincipalDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,7 +17,15 @@ public class UserController {
     }
 
     @GetMapping("/user/{id}/update")
-    public String update(@PathVariable int id){
+    public String update(@PathVariable int id, @AuthenticationPrincipal PrincipalDetails principalDetails){
+        // 1. 어노테이션을 이용한 쉬운 방법
+        System.out.println("세션 정보 : " + principalDetails.getUser());
+
+        // 2. 복잡하고 어려운 방법
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        PrincipalDetails mPrincipalDetails = (PrincipalDetails)auth.getPrincipal();
+        System.out.println("직접 찾은 세션 정보 : " + mPrincipalDetails.getUser());
+
         return "user/update";
     }
 }
