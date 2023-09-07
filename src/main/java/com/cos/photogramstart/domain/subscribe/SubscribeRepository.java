d7417b80f8121ea4eddb5411f122810aa1c34b17
Legday <chlwodud0327@naver.com>
2023-09-07 19:36:19 +0900

    Subscribe API 설계(Native Query)

--- src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -1,7 +1,20 @@
 package com.cos.photogramstart.domain.subscribe;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 
+    @Modifying // 데이터베이스에 변경을 주는 쿼리(INSERT,DELETE,UPDATE)에는 @Modifying 어노테이션이 필요하다
+    @Query(value =
+            "INSERT INTO subscribe(fromUserId, toUserId,createdDate) VALUES(:fromUserId, :toUserId, now())",
+            nativeQuery = true)
+    void mSubscribe(int fromUserId, int toUserId); // 성공하면 1(변경된 행의 갯수) , 실패하면 -1 Return
+
+    @Modifying
+    @Query(value =
+            "DELETE FROM subscribe WHERE fromUserId=:fromUserId AND toUserId=:toUserId",
+            nativeQuery = true)
+    void mUnSubscribe(int fromUserId, int toUserId);
 }
