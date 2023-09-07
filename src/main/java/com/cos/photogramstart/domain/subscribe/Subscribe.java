bd085701e1278503302fb9055b27853c939202f5
Legday <chlwodud0327@naver.com>
2023-09-07 19:08:01 +0900

    Subscribe 도메인 설계

---
+++ src/main/java/com/cos/photogramstart/domain/subscribe/Subscribe.java
@@ -0,0 +1,45 @@
+package com.cos.photogramstart.domain.subscribe;
+
+import com.cos.photogramstart.domain.user.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name="subscribe_uk",
+                        columnNames = {"fromUserId","toUserId"} //실제 DB 컬럼명을 적어야한다.
+                )
+        }
+)
+public class Subscribe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "fromUserId")
+    private User fromUser;
+
+    @ManyToOne
+    @JoinColumn(name = "toUserId")
+    private User toUser;
+
+    private LocalDateTime createDate;
+
+    @PrePersist // DB에 INSERT 되기 직전에 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
