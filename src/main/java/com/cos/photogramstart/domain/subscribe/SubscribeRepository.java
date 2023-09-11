27cd41f5bffcee7a412a56e811fc6911b16c1e1d
Legday <chlwodud0327@naver.com>
2023-09-11 13:48:54 +0900

    Native Query 수정

--- src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
+++ src/main/java/com/cos/photogramstart/domain/subscribe/SubscribeRepository.java
@@ -8,7 +8,7 @@ public interface SubscribeRepository extends JpaRepository<Subscribe, Integer> {
 
     @Modifying // 데이터베이스에 변경을 주는 쿼리(INSERT,DELETE,UPDATE)에는 @Modifying 어노테이션이 필요하다
     @Query(value =
-            "INSERT INTO subscribe(fromUserId, toUserId,createdDate) VALUES(:fromUserId, :toUserId, now())",
+            "INSERT INTO subscribe(fromUserId, toUserId,createDate) VALUES(:fromUserId, :toUserId, now())",
             nativeQuery = true)
     void mSubscribe(int fromUserId, int toUserId); // 성공하면 1(변경된 행의 갯수) , 실패하면 -1 Return
 
