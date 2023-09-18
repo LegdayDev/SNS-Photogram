8ed17ac6d1196242b92e65f074131b94399fa510
Legday <chlwodud0327@naver.com>
2023-09-18 19:11:29 +0900

    좋아요 기능 랜더링 페이지 수정, 좋아요 도메인 구현

---
+++ src/main/java/com/cos/photogramstart/domain/likes/Likes.java
@@ -0,0 +1,44 @@
+package com.cos.photogramstart.domain.likes;
+
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name="likes_uk",
+                        columnNames = {"imageId","userId"} //실제 DB 컬럼명을 적어야한다.
+                )
+        }
+)
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "imageId")
+    private Image image;
+
+    // 오류가 터지고 나서 @JsonIgnoreProperties 를 해주자.
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    private LocalDateTime createDate;
+
+    @PrePersist // DB에 INSERT 되기 직전에 실행
+    public void createDate(){
+        this.createDate = LocalDateTime.now();
+    }
+}
