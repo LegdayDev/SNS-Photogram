d750407c71ae4187fb70e1867139b84d99cd62ae
Legday <chlwodud0327@naver.com>
2023-09-25 17:07:18 +0900

    OAuth - 페이스북 로그인화면 연결

--- src/main/java/com/cos/photogramstart/config/oauth/OAuth2DetailsService.java
+++ src/main/java/com/cos/photogramstart/config/oauth/OAuth2DetailsService.java
@@ -1,9 +1,19 @@
 package com.cos.photogramstart.config.oauth;
 
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OAuth2DetailsService extends DefaultOAuth2UserService {
     //페이스북서버에서 응답으로 온 유저정보에 대한 로직을 짠다.
+
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        return super.loadUser(userRequest);
+    }
 }
