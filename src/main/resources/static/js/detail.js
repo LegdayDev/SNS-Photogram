5922291755bfeec9a92924b6c68aa404c51db143
Legday <chlwodud0327@naver.com>
2023-10-08 14:36:12 +0900

    detail.js 리팩토링

--- src/main/resources/static/js/detail.js
+++ src/main/resources/static/js/detail.js
@@ -1,4 +1,20 @@
-// 이미지삭제(imageId,userId)
+/**
+ * 1) 이미지삭제(imageId, userId)
+    - imageId 를 통해 이미지 삭제하고 외래키로 연결된 엔티티 같이 삭제
+    - userId 로 user 프로필 페이지로 이동
+ * 2) 이미지 업데이트(imageId, pageUserId, principalId)
+    - FormData 를 이용하여 수정할 이미지와 caption(내용)을 저장
+    - FormData 로 image 엔티티 수정 후 이미지 상세 화면으로 이동
+ * 3) 이미지 업로드 후 페이지로드 없이 반영
+    - pageUserId(이미지 작성 유저) 와 principalId(로그인한 유저) 를 비교하여 다르면 수정할 수없게 막는다.
+    - 맞다면 모달창을 띄어서 이미지 업로드를 구현
+ * 4) 사진 업로드, 취소 모달창
+ * 5) 사용자 정보 메뉴 열기
+ * 6) 사용자 정보 메뉴 닫기
+ */
+
+
+// 1) 이미지삭제(imageId,userId)
 function userStoryDelete(imageId,userId){
     if(confirm("삭제 하시겠습니까 ?")){
         $.ajax({
@@ -17,7 +33,7 @@ function userStoryDelete(imageId,userId){
 
 }
 
-// 실제 업데이트 정보(image, caption) 서버로 전송
+// 2) 이미지 업데이트(imageId, pageUserId, principalId)
 function userStoryUpdate(imageId, pageUserId, principalId) {
     let formData = new FormData($("#userImageForm")[0]);
     formData.append("caption",$("#userCaption").val());
@@ -41,7 +57,7 @@ function userStoryUpdate(imageId, pageUserId, principalId) {
 
 }
 
-// 수정된 이미지 반영
+// 3) 이미지 업로드 후 페이지 로드 없이 반영
 function imageUpdate(imageId, pageUserId, principalId) {
     if (pageUserId != principalId) {
         alert("사진을 수정할 수 없는 유저입니다.");
@@ -66,16 +82,16 @@ function imageUpdate(imageId, pageUserId, principalId) {
     });
 }
 
-// (6) 사용자 프로파일 이미지 메뉴(사진업로드, 취소) 모달
+// 4) 사진 업로드, 취소 모달창
 function modalImage() {
     $(".modal-image").css("display", "none");
 }
 
-// (4) 사용자 정보 메뉴 열기 닫기
+// 5) 사용자 정보 메뉴 열기
 function popup(obj) {
     $(obj).css("display", "flex");
 }
-
+// 6) 사용자 정보 메뉴 닫기
 function closePopup(obj) {
     $(obj).css("display", "none");
 }
